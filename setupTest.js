978216986e4899791d448a21bf44f09b63ac414d
Humberto <havila@novasolutionsystems.com>
2020-08-25 17:48:41 -0500

    feat: add configuration for unit test

--- setupTest.js
+++ setupTest.js
@@ -1,6 +1,32 @@
-import { configure } from 'enzyme';
+// import { configure } from 'enzyme';
+import Ensyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 
-configure({ adapter: new Adapter() });
+Ensyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('', { url: 'https://localhost' });
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.requestAnimationFrame = function (callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function (id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
