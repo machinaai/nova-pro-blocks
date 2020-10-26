32b2b7bc59ca58fa70a928460d7212460f95c2e6
Sandra <samontiel@novasolutionsystems.com>
2020-10-26 17:23:49 -0600

    feat():Add splash screen problock

---
+++ RaoSplashScreen/src/blocks/carousel/locales/es-ES/carousel.ts
@@ -0,0 +1,4 @@
+export default {
+  'carousel.sign_up': 'Regístrate',
+  'carousel.next': 'Siguiente',
+};
---
+++ RaoSplashScreen/src/interfaces/splash-screen.tsx
@@ -0,0 +1,21 @@
+/**
+ * Interface of definition for the component
+ */
+export interface PropsCarousel {
+    options: ItemOption[];
+    redirect:string,
+}
+interface ItemOption {
+    img: string,
+    valH1: React.ReactNode,
+    valH3: React.ReactNode
+}
+
+
+export interface PropsProblock{
+    imagesCarousel:string[];
+    logo1:string;
+    logo2:string;
+    iconNextButton:string;
+    iconCircle:string;
+}
