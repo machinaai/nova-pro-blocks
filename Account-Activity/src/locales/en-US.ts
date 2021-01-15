89fdf1a8de2fb7eac125d11f14fbafdf2f33ced4
Sandra <samontiel@novasolutionsystems.com>
2021-01-15 17:35:57 -0600

    feat():add account activity pro-block

---
+++ Account-Activity/src/locales/en-US.ts
@@ -0,0 +1,18 @@
+export default {
+  'BLOCK_NAME_CAMMEL_CASE.optionCheck1': 'less than',
+  'BLOCK_NAME_CAMMEL_CASE.optionCheck2': 'more of',
+  'BLOCK_NAME_CAMMEL_CASE.modalTitle': 'Continue with INE/IFE',
+  'BLOCK_NAME_CAMMEL_CASE.modalSubtitle': 'Select an option',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption1': 'Scan document',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption2': 'Upload document',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption3': 'Upload data manually',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadTitle': 'Upload document',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadSubtitle': 'Valid formats',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadOp1': 'Photos',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadOp2': 'To explore',
+  'BLOCK_NAME_CAMMEL_CASE.title': 'Account activity',
+  'BLOCK_NAME_CAMMEL_CASE.subtitle': 'How much do you estimate to deposit monthly to your account?',
+  'BLOCK_NAME_CAMMEL_CASE.terms-conditions1': 'Accept the',
+  'BLOCK_NAME_CAMMEL_CASE.terms-conditions2': 'Terms and conditions',
+  'BLOCK_NAME_CAMMEL_CASE.continue_button': 'Continue'
+};
---
+++ Account-Activity/src/locales/es-ES.ts
@@ -0,0 +1,18 @@
+export default {
+  'BLOCK_NAME_CAMMEL_CASE.optionCheck1': 'Menos de',
+  'BLOCK_NAME_CAMMEL_CASE.optionCheck2': 'Más de',
+  'BLOCK_NAME_CAMMEL_CASE.modalTitle': 'Continuar con INE/IFE',
+  'BLOCK_NAME_CAMMEL_CASE.modalSubtitle': 'Selecciona una opción',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption1': 'Escanear documento',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption2': 'Subir documento',
+  'BLOCK_NAME_CAMMEL_CASE.modalOption3': 'Cargar datos manualmente',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadTitle': 'Subir documento',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadSubtitle': 'Formatos válidos',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadOp1': 'Fotos',
+  'BLOCK_NAME_CAMMEL_CASE.drawerUploadOp2': 'Explorar',
+  'BLOCK_NAME_CAMMEL_CASE.title': 'Actividad de la cuenta',
+  'BLOCK_NAME_CAMMEL_CASE.subtitle': '¿Cuánto estimas depositar mensualmente a tu cuenta?',
+  'BLOCK_NAME_CAMMEL_CASE.terms-conditions1': 'Acepto los',
+  'BLOCK_NAME_CAMMEL_CASE.terms-conditions2': 'Términos y condiciones',
+  'BLOCK_NAME_CAMMEL_CASE.continue_button': 'Continuar'
+};
