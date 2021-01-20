a9c1bf9cf92f4316bee37f4bbd652481433095ee
raquell_vazquez <lvazquez@novasolutionsystems.com>
2021-01-20 17:39:21 -0600

    feature: create pro block to total request component

---
+++ TotalRequest/src/locales/en-US.ts
@@ -0,0 +1,9 @@
+export default {
+  'BLOCK_NAME.title': 'Total requests',
+  'BLOCK_NAME.subtitle': 'Status of requests',
+  'BLOCK_NAME.op1': 'Initiates',
+  'BLOCK_NAME.op2': 'In process',
+  'BLOCK_NAME.op3': 'Abandoned',
+  'BLOCK_NAME.op4': 'Finished',
+  'BLOCK_NAME.titleTooltip': 'Help'
+};
