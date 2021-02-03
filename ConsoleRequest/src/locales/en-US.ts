c79f6dcd8a0d13860f91c3602779090645259d15
Sandra <samontiel@novasolutionsystems.com>
2021-02-02 18:03:15 -0600

    feat():add console-request template

---
+++ ConsoleRequest/src/locales/en-US.ts
@@ -0,0 +1,8 @@
+export default {
+    'BLOCK_NAME.titleRequest':'Awaiting requests',
+    'BLOCK_NAME.RadioBtnOp1':'All',
+    'BLOCK_NAME.RadioBtnOp2':'Documentation',
+    'BLOCK_NAME.RadioBtnOp3':'Validation',
+    'BLOCK_NAME.RadioBtnOp4':'Beneficiaries',
+    'BLOCK_NAME.InputTxt':'Search by: number, name',  
+   };
