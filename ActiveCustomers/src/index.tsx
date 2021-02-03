b9a4af0b1e877d040122c577ebd358482d33f368
Sandra <samontiel@novasolutionsystems.com>
2021-02-03 17:40:38 -0600

    feat():add active-customer template

---
+++ ActiveCustomers/src/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { connect, useDispatch } from 'umi';
+import ActiveCustomer from './pro-blocks/active-customer/index';
+import {StateModelCustomers} from './pro-blocks/active-customer/models/model'
+
+const PAGE_NAME_UPPER_CAMEL_CASE: React.FC = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({
+            type: 'activeCustomer/setRoutePath',
+            payload: '/client-detail'
+        });
+    }, []);
+
+    return (
+        <div>
+            <ActiveCustomer />
+        </div>
+    )
+}
+export default connect(({activeCustomer}:{activeCustomer: StateModelCustomers})=>({
+    }))(PAGE_NAME_UPPER_CAMEL_CASE);
